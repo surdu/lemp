af1310bcf1868942b157bd3e893dcd31629ac98f
surdu <postal2600@gmail.com>
2017-03-27 03:16:54 +0300

    first commit

---
+++ app/views/deviceList/deviceList.js
@@ -0,0 +1,35 @@
+var observableModule = require("data/observable");
+var ObservableArray = require("data/observable-array").ObservableArray;
+
+var bt = require("nativescript-bluetooth");
+
+var devices = new ObservableArray([]);
+
+var pageData = new observableModule.fromObject({
+	deviceList: devices
+});
+
+function scan() {
+	console.log("Scanning...");
+	devices.length = 0;
+	bt.startScanning({
+		serviceUUIDs: ["1800"],
+		seconds: 60,
+		onDiscovered: function (peripheral) {
+			devices.push(peripheral)
+			console.log("Periperhal found with UUID: " + peripheral.UUID);
+		}
+	}).then(function() {
+		console.log("Scanning complete");
+	}, function (err) {
+		console.log("error while scanning: " + err);
+	});
+}
+
+module.exports = {
+	loaded: function(args) {
+		var page = args.object;
+		page.bindingContext = pageData;
+	},
+	scan: scan
+}
